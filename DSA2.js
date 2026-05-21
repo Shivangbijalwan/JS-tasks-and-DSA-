781f2d3f1b840394c6b24bb13187c6945196c2ee
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-22 00:59:17 +0530

    updated code

--- DSA2.js
+++ DSA2.js
@@ -1,2 +1,11 @@
 // this is DSA 2 STRINGS in js 
 
+let s = "            shivang          "
+
+console.log(s.slice(0,5));
+
+console.log(s.concat(" world"));
+
+console.log(s.trim(s));
+
+console.log(s.charCodeAt(2));
