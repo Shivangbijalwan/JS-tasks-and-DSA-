a95df35e39fd6b0e80af0e58697b5ac62bc8dd47
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-26 00:15:20 +0530

    bubble sorting

---
+++ 34DSA_sorting.js
@@ -0,0 +1,15 @@
+//1- BUBBLE SORTING
+
+//how it works for ascending order
+
+//[10 , 5, 1, 12, 3]
+// let 1st element or 0 index number is bubble element (here "10" is bubble element)
+// swap to the right one if its bigger than that 
+// if its not bigger than right element we have to pass to right element
+// now 12 is the bubble element 
+// swap to the right one if its bigger than that
+// if bubble element is at its perfect place  the perfect place is last index of the array
+// phase 2 has been started as any one bubble has been fixed in its place 
+// now new phase to be started till phase-1
+// make a new bubble element which is in the 0 index
+// repeat this all till it sort (in ascending order)
