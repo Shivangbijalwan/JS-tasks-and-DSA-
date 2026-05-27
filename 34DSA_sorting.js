2a42ddf2a66c6f1cc648466bd63b765d4d9e2d37
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-27 17:38:27 +0530

    updated sorting in js and added now working on pattern program in js

--- 34DSA_sorting.js
+++ 34DSA_sorting.js
@@ -13,3 +13,76 @@
 // now new phase to be started till phase-1
 // make a new bubble element which is in the 0 index
 // repeat this all till it sort (in ascending order)
+
+// code for bubble sort
+/* 
+let arr = [10, 5, 1, 12, 3];
+for(let i=0 ; i<arr.length-1 ; i++){
+  for(let j=0 ; j<arr.length-1-i; j++ ){
+     if(arr[j]>arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+     }
+  }
+}
+console.log(arr)
+*/
+
+
+//2 - SELECTION SORTING
+
+// let [10, 5, 1, 12, 9]
+
+//inside the array start from 1st index to last
+//find the smallest element inside the unsorted array
+// swap it from 0th index if its small then it 
+// repeat till end
+
+//code for Selection sorting
+
+/*
+let arr = [10, 5, 1, 12, 9];
+
+for(let i=0 ; i<arr.length-1 ; i++){
+    let min = i;
+    for(let j=i+1 ; j<arr.length ; j++){
+      if(arr[min]>arr[j]){
+       min = j ;
+
+      }
+      if (min != i){
+        let temp = arr[min];
+        arr[min] = arr[i]
+        arr[i] = temp
+      }
+    }
+}
+console.log(arr);
+*/
+
+
+
+//3 - INSERTION SORTING
+
+//arr = [5, 10, 1, 8, 13, 7]
+//start from 1st index  (i) and its a key
+// 2nd pointer from 0th index or from i-1 (j) 
+// if j element is > then key
+// j+1 == j
+//j-- 
+// check if j = 0 && <0
+//j++
+//j+1 = key
+
+let arr = [5, 10, 1, 8, 13, 7];
+for (let i = 1 ; i < arr.length ; i++){
+    let key = arr[i];
+    let j = i - 1;
+    while(j >= 0 && arr[j] > key){
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = key;
+}
+console.log(arr);
