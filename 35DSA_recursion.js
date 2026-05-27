f377679c8c0c01adfb9ca8df379c487014357180
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-28 00:50:25 +0530

    completed recursion and call stack questions and learning

---
+++ 35DSA_recursion.js
@@ -0,0 +1,41 @@
+// This is Recursion in DSA 
+
+// A function calling again and again until it reaches a end point
+
+/*
+// n to 1
+function num(n) { 
+    if(n==0) {
+        return;
+    }
+    console.log(n);
+    num(n-1);
+}
+
+num(prompt("Enter a number: "));
+*/
+
+/*
+// 1 to n backtracking 
+function num(n) { 
+    if(n==0) {
+        return;
+    }
+    num(n-1);
+    console.log(n);
+    
+}
+
+num(prompt("Enter a number: "));
+*/
+
+//sum of n numbers
+/*
+function sum(n){
+    if(n==1) return 1;  
+    return n + sum(n-1);
+}
+console.log(sum(5))
+*/
+
+
