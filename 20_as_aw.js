2ceca44faa2075dd2db5e8da5337022517b2f18a
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-06-01 23:37:42 +0530

    updated

--- 20_as_aw.js
+++ 20_as_aw.js
@@ -1,40 +1,7 @@
-// this is ASYNC / AWAIT and fetching API's
-console.log("this is async");
+// synchronous - one bye one doing a group of work till the 1st 
+//                  work is not completed it dose not start another .
 
-async function getData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(445);
-    }, 5000);
-  });
-}
-async function main() {
-  console.log("loading modules");
-
-  console.log("fetching the data");
-
-  let data = await getData();
-
-  console.log(data);
-
-  console.log("processing data");
-  console.log("re-fetching the data");
-}
-
-main()
-
-//fetching the data
-
-// fetch(url, options)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.error('Error:', error));
+// asynchronous - starting a group of work in one time but response giving to user 
+//                  is depend on completion  of the particular work . 
 
 
-
-
-// data.then((v)=> {
-//        console.log("processing data");
-//           console.log(data);
-//             console.log("re-fetching the data");
-// })
