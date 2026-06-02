a5e50ba574946ad508cd7c9e4388f93ca0781584
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-06-02 23:59:27 +0530

    code updated

--- 38merge_sorting.js
+++ 38merge_sorting.js
@@ -1,1 +1,50 @@
-// this is merge sorting 
+// Merge Sort
+// Divide and Conquer
+
+function conquer(arr, first, mid, last) {
+    let temp = new Array(last - first + 1);
+
+    let i = first;
+    let j = mid + 1;
+    let k = 0;
+
+    while (i <= mid && j <= last) {
+        if (arr[i] < arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= last) {
+        temp[k++] = arr[j++];
+    }
+
+    let p = 0;
+    let t = first;
+
+    while (p < temp.length) {
+        arr[t++] = temp[p++];
+    }
+}
+
+function divide(arr, first, last) {
+    if (first >= last) return;
+
+    let mid = Math.floor((first + last) / 2);
+
+    divide(arr, first, mid);
+    divide(arr, mid + 1, last);
+
+    conquer(arr, first, mid, last);
+}
+
+let arr = [8, 2, 1, 9, 5, 12, 4, 20];
+
+divide(arr, 0, arr.length - 1);
+
+console.log(arr);
